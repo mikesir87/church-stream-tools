0d287b4dbddc9c64a3d5074db6460e56cae6fe79
Michael Irwin <mikesir87@gmail.com>
2023-03-05 10:10:58 -0500

    Restore show name button

--- controller/src/components/stream/SceneChanger.tsx
+++ controller/src/components/stream/SceneChanger.tsx
@@ -5,6 +5,7 @@ import { useStreamProps } from "./StreamContext";
 import { BigButton } from "./BigButton";
 import { HymnButtons } from "./HymnButtons";
 import { ShowHymnButton } from "./ShowHymnButton";
+import { ShowName } from "./ShowName";
 
 export const SceneChanger : FC = () => {
     return (
@@ -27,7 +28,7 @@ const SceneChangingButtons : FC = () => {
         return (
             <>
                 <ShowHymnButton />
-                {/* <ShowName /> */}
+                <ShowName />
                 <BigButton text="Pause Stream" onClick={startSacramentPause} />
                 <BigButton text="End Stream" variant="danger" onClick={endStream} requireVerification />
             </>
